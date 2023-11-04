ce371893e1ede9cd086b47fbf4e5e179b4bc1022
owen <owen@owen>
2023-11-04 23:42:20 +0000

    design

--- packages/nextjs/pages/create-organisation.tsx
+++ packages/nextjs/pages/create-organisation.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { MetaHeader } from "~~/components/MetaHeader";
 import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 export default function CreateOrganisation() {
@@ -21,49 +22,52 @@ export default function CreateOrganisation() {
 
   // render the form and the status
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold text-primary-content">Start using Chipper</h1>
+    <>
+      <MetaHeader title="Create Organisation" />
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold text-primary-content">Start using Chipper</h1>
 
-      <h3 className="text-primary-content mt-4">Want to accelerate the development of your new idea? Let's go!</h3>
+        <h3 className="text-primary-content mt-4">Want to accelerate the development of your new idea? Let's go!</h3>
 
-      <br />
+        <br />
 
-      <div className="flex flex-col mt-5">
-        <label htmlFor="name" className="text-lg font-medium text-accent">
-          Name
-        </label>
-        <input
-          id="name"
-          type="text"
-          value={name}
-          onChange={e => setName(e.target.value)}
-          className="border border-gray-300 p-2 rounded mt-2"
-          required
-        />
+        <div className="flex flex-col mt-5">
+          <label htmlFor="name" className="text-lg font-medium text-accent">
+            Name
+          </label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={e => setName(e.target.value)}
+            className="border border-gray-300 p-2 rounded mt-2"
+            required
+          />
+        </div>
+        <div className="flex flex-col mt-5">
+          <label htmlFor="website" className="text-lg font-medium text-accent">
+            Website
+          </label>
+          <input
+            id="website"
+            type="url"
+            value={website}
+            onChange={e => setWebsite(e.target.value)}
+            className="border border-gray-300 p-2 rounded mt-2"
+            required
+          />
+        </div>
+
+        <button
+          onClick={() => writeAsync()}
+          className="mt-5 bg-primary hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+          disabled={isLoading}
+        >
+          {isLoading ? "Creating..." : "Create"}
+        </button>
+        {isError && <p className="text-red-500 font-medium mt-4">Organisation creation failed: {error?.message}</p>}
       </div>
-      <div className="flex flex-col mt-5">
-        <label htmlFor="website" className="text-lg font-medium text-accent">
-          Website
-        </label>
-        <input
-          id="website"
-          type="url"
-          value={website}
-          onChange={e => setWebsite(e.target.value)}
-          className="border border-gray-300 p-2 rounded mt-2"
-          required
-        />
-      </div>
-
-      <button
-        onClick={() => writeAsync()}
-        className="mt-5 bg-primary hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-        disabled={isLoading}
-      >
-        {isLoading ? "Creating..." : "Create"}
-      </button>
-      {isError && <p className="text-red-500 font-medium mt-4">Organisation creation failed: {error?.message}</p>}
-    </div>
+    </>
   );
 }
 // import the useCreateOrganisation hook
