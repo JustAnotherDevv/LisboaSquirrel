672452fd2f5d0ce4553a8231b4c8208b2c88fdf6
Dercio <contact@creedscode.cc>
2023-11-04 20:54:54 +0100

    Add Ui changes

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -12,24 +12,18 @@ const Home: NextPage = () => {
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <Image src="/logo.svg" alt="Logo" width={200} height={200} />
 
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-          <Image src="/logo.svg" alt="Logo" width={200} height={200} />
-
-          <div className="flex space-x-4 justify-center items-center h-screen">
-            <Link
-              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-              href="/create-organisation"
-            >
-              Create an Organisation
-            </Link>
-            <Link className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded" href="/dashboard">
-              Go to Dashboard
-            </Link>
-          </div>
+        <div className="flex space-x-4 justify-center items-center h-screen">
+          <Link
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+            href="/create-organisation"
+          >
+            Create an Organisation
+          </Link>
+          <Link className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded" href="/dashboard">
+            Go to Dashboard
+          </Link>
         </div>
-      </div>
-
-      <FirstChart width={400} height={400} />
+      </div >
     </>
   );
 };
