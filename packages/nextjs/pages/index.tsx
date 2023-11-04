9de4db7eccb07338753c584133516618c9ba11ad
owen <owen@owen>
2023-11-04 14:47:57 +0000

    chart example

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
+import FirstChart from "~~/components/FirstChart";
 import { MetaHeader } from "~~/components/MetaHeader";
 
 const Home: NextPage = () => {
   return (
     <>
       <MetaHeader />
+      <FirstChart width={400} height={400} />
     </>
   );
 };
